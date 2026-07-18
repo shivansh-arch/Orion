af3f99e716c7e733c48539fa1a933d050f3f4da7
Shivansh <shivansh.gupta2024@lpu.in>
2026-07-18 07:23:15 +0530

    Update client.py

--- src/client.py
+++ src/client.py
@@ -26,7 +26,7 @@ class OrionClient:
             api_key=api_key,
         )
 
-        self.model="gpt-oss-120b"
+        self.model = "gpt-oss-120b"
 
     def chat(self, messages, temperature=0.7, max_tokens=1000):
         response = self.client.chat.completions.create(
@@ -35,7 +35,12 @@ class OrionClient:
             temperature=temperature,
             max_tokens=max_tokens,
         )
-        return response.choices[0].message.content
+
+        content = response.choices[0].message.content
+        if content is None:
+            content = ""
+
+        return content
 
     def chat_with_tools(self, messages, tools, temperature=0.7, max_tokens=1000):
         response = self.client.chat.completions.create(
@@ -45,4 +50,5 @@ class OrionClient:
             temperature=temperature,
             max_tokens=max_tokens,
         )
+
         return response.choices[0].message
